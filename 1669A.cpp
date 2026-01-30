9980c1c0c40cff5febdf5a73207eb602bb45a559
Prabhjot Singh Sohal <154662377+prabhjot26-jangli@users.noreply.github.com>
2026-01-30 14:09:49 +0530

    Solved 1669A

---
+++ 1669A.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        int rating = 0;
+        cin >> rating;
+        if (rating <= 1399)
+        {
+            cout << "Division 4" << endl;
+        }
+        else if (rating >= 1400 && rating <= 1599)
+        {
+            cout << "Division 3" << endl;
+        }
+        else if (rating >= 1600 && rating <= 1899)
+        {
+            cout << "Division 2" << endl;
+        }
+        else if (rating >= 1900)
+        {
+            cout << "Division 1" << endl;
+        }
+    }
+    return 0;
+}
