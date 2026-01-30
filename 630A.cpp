d597ef6bd6dc8c23a228fae68b5b6acf580ec78f
Prabhjot Singh Sohal <154662377+prabhjot26-jangli@users.noreply.github.com>
2026-01-30 14:09:13 +0530

    Solved 630A

---
+++ 630A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k = 0;
+    cin >> k;
+    if (k == 1)
+        cout << "05";
+    else
+        cout << "25";
+
+    return 0;
+}
