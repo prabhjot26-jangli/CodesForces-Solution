adf13e57f1e1cf7b5412833ef3ade98d37e80236
Prabhjot Singh Sohal <154662377+prabhjot26-jangli@users.noreply.github.com>
2026-02-06 16:21:28 +0530

    Constructor University Challange

---
+++ cafeteria.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n][2];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    return 0;
+}
