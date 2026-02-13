4c3c1ad6274c5550d65806714160b7de20456e4d
Prabhjot Singh Sohal <154662377+prabhjot26-jangli@users.noreply.github.com>
2026-02-13 23:58:47 +0530

    Solved 1294A

---
+++ 1294A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, n;
+        cin >> a >> b >> c >> n;
+
+
+
+        
+    }
+
+    return;
+}
