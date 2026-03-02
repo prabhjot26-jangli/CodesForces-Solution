601078a664a3939fb112a541d5e2a96cfc21cc19
Prabhjot Singh Sohal <154662377+prabhjot26-jangli@users.noreply.github.com>
2026-03-02 23:45:49 +0530

    Solved 2131A

---
+++ 2131A.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> a(n), b(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        for (int i = 0; i < n; i++)
+            cin >> b[i];
+
+        long long pos = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > b[i])
+                pos += (a[i] - b[i]);
+        }
+
+        if (pos == 0)
+            cout << 1 << endl;
+        else
+            cout << pos + 1 << endl;
+    }
+
+    return 0;
+}
